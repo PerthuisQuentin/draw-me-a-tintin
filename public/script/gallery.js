9c08bbb6f62e4fe25cd642174e6083e25879c901
PerthuisQuentin <perthuis.quentin@gmail.com>
2017-05-12 01:14:28 +0200

    :lipstick: [Gallery] Fix images modal

--- public/script/gallery.js
+++ public/script/gallery.js
@@ -1,33 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-	var modal = document.getElementById("images-modal");
-	var modalImg = document.getElementById("modal-image");
+	var imagesModal = $("#images-modal");
+	var modalImage = imagesModal.find("#images-modal-image");
 
 	var images = document.getElementsByClassName("image-container");
-	var imageHeader = document.getElementsByClassName("image-container-header")[0];
-	var modalCloseButton = document.getElementsByClassName("close")[0];
-	var modalContent = document.getElementsByClassName("modal-content")[0];
 
-	function closeModal() {
-		modal.style.display = "none";
-	}
-
-	modalCloseButton.addEventListener("click", closeModal);
-
-	modalContent.addEventListener("click", function(e) {
-		if(e.target == this) closeModal();
+	$(".image-container").click(function(e) {
+		modalImage.attr('src', $(this).find('.image').attr('src'));
+		imagesModal.modal('show');
 	});
 
-
-
-	function openModal() {
-		modal.style.display = "block";
-	 	modalImg.src = this.getElementsByClassName("image")[0].src
-	}
-
-	for(var i = 0; i < images.length; i++) {
-		images[i].addEventListener("click", openModal, true);
-	}
-
-	imageHeader.addEventListener("click", openModal, true);
+	imagesModal.find(".images-modal-content").click(function(e) {
+		if(e.target == this) {
+			imagesModal.modal('hide');
+		}
+	});
 });
