0204fd74fe3bc52ed5c53e07af7a633d5ae74c95
PerthuisQuentin <perthuis.quentin@gmail.com>
2017-05-23 01:20:34 +0200

    :sparkles: [Models] Add a model for users

---
+++ lib/models/users.ts
@@ -0,0 +1,22 @@
+const Mongoose = require('mongoose');
+const Bcrypt = require('bcrypt');
+
+var userSchema = Mongoose.Schema({
+    local            : {
+        email        : String,
+        password     : String,
+    },
+    username: String,
+    role: String
+});
+
+userSchema.methods.generateHash = function(password: string) {
+    return Bcrypt.hashSync(password, Bcrypt.genSaltSync(8), null);
+};
+
+// checking if password is valid
+userSchema.methods.validPassword = function(password: string) {
+    return Bcrypt.compareSync(password, this.local.password);
+};
+
+export default Mongoose.model('Users', userSchema);
