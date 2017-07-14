3c7415cc8b89d30a73beb356ce7327f0d89fd67b
PerthuisQuentin <perthuis.quentin@gmail.com>
2017-07-15 01:30:30 +0200

    :sparkles: [Models] Add database model for images

---
+++ lib/models/images.ts
@@ -0,0 +1,35 @@
+import * as Mongoose from 'mongoose';
+
+import { IUser } from './users';
+
+export interface IImage {
+    file: String,
+    name: String,
+    author: IUser,
+    creationDate: Date
+}
+
+export interface IImageModel extends IImage, Mongoose.Document {
+	toPublicObject(): IImage;
+}
+
+var imageSchema: Mongoose.Schema = new Mongoose.Schema({
+	file: String,
+	name: String,
+	author: { type: Mongoose.Schema.Types.ObjectId, ref: 'User' },
+	creationDate: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+imageSchema.methods.toPublicObject = function(): IImage {
+	return {
+		file: this.file,
+		name: this.name,
+		author: this.author,
+		creationDate: this.creationDate
+	};
+};
+
+export const Images: Mongoose.Model<IImageModel> = Mongoose.model<IImageModel>('User', imageSchema);
